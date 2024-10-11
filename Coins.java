c5b81329310193b7678c88f886942d1a6ff23c17
Karlp-Web <Karlenportillo1@student.hbuhsd.edu>
2024-10-11 10:46:47 -0700

    yomomma

--- Coins.java
+++ Coins.java
@@ -1,14 +1,23 @@
 public class Coins
 {
-  public static void main(String[] args)
-  {
-    
-    
-    int coinConverter = ;
+  public static void main(String[] args){
+  Scanner scanner = new Scanner(System.in);//creates a scanner object
 
-    System.out.println();
+  System.out.print("Enter the number of pennies");//prompt the user
+  in pennies = scanner.nextInt();// Read the user input
+
+  coinConverter(pennies);//call the method with user input
+
+  scanner.close();//Close the scanner
+  }
+  
+    public static void coinConverter(int pennies){
+    
+
+
+
   }
 
-  // write code for coinConverter
+  
 
 }
